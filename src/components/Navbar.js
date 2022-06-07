44260ffd7b9d04936fdd19caa1eb91ce27f432c1
DxDougie <91690165+DxDougie@users.noreply.github.com>
2022-06-07 18:16:05 -0300

    atualizando

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img
+          alt="pokemon-logo"
+          src="../assest/pokemon_logo"
+          className="navbar-img"
+        />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
